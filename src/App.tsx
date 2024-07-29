eb14e804b2970f7af1993add1bdc061c37040d14
Aayush Thakur <ashcold21@gmail.com>
2024-07-30 00:05:18 +0530

    Finalize the app::

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,63 @@
-import InputBox from "./components/InputBox"
+import { useState } from "react";
+import InputBox from "./components/InputBox";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(0)
+    setAmount(convertedAmount)
+  }
+
+  const convertCurrency = () => {
+    setConvertedAmount(amount * (currencyInfo[to] as number));
+  }
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center bg-[url('https://images.pexels.com/photos/163036/mario-luigi-yoschi-figures-163036.jpeg')] bg-cover">
       <div className="max-w-lg w-full p-8 bg-blue-300 bg-opacity-25 flex flex-col gap-4 justify-center items-center rounded-lg relative">
-        <InputBox />
-        <button className="w-fit bg-blue-500 hover:bg-blue-700 text-yellow-50 rounded-md px-2 py-1 border-2 border-white absolute left-1/2 -translate-x-1/2 -translate-y-7">
-        swap
+        <InputBox
+          label="From"
+          amount={amount}
+          onAmountChange={setAmount}
+          onCurrencyChange={setFrom} // This might cause some issue
+          currencyOptions={options}
+          selectCurrency={from}
+        />
+        <button
+          className="w-fit bg-blue-500 hover:bg-blue-700 text-yellow-50 rounded-md px-2 py-1 border-2 border-white absolute left-1/2 -translate-x-1/2 -translate-y-7"
+          onClick={swap}
+        >
+          swap
         </button>
-        <InputBox />
-        <button className="w-full p-2 bg-blue-500 hover:bg-blue-600 rounded-md text-yellow-50">
-          Convert USD to INR
+        <InputBox
+          label="To"
+          amount={convertedAmount}
+          onAmountChange={setAmount}
+          onCurrencyChange={setTo} // This might cause some issue
+          currencyOptions={options}
+          selectCurrency={to}
+          amountDisable
+        />
+        <button
+          className="w-full p-2 bg-blue-500 hover:bg-blue-600 rounded-md text-yellow-50"
+          onClick={convertCurrency}
+        >
+          Convert {from.toUpperCase()} to {to.toUpperCase()}
         </button>
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,14 +1,51 @@
-const InputBox = () => {
+import { useId } from "react"
+
+interface InputBoxProps {
+  label: string;
+  amount: number;
+  onAmountChange: (amount: number) => void;
+  onCurrencyChange: (currency: string) => void;
+  currencyOptions?: string[];
+  selectCurrency?: string;
+  amountDisable?: boolean;
+}
+
+const InputBox = ({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false
+}: InputBoxProps) => {
+  const amountInputId = useId()
   return (
     <div className="w-full max-h-min rounded-lg p-4 bg-white flex">
       <div className="w-1/2 flex flex-col justify-between gap-4">
-        <label htmlFor="from">From</label>
-        <input className="px-2 py-2 rounded-md bg-stone-200" type="number" id="from"/>
+        <label htmlFor={amountInputId}>{label}</label>
+        <input
+          className="px-2 py-2 rounded-md bg-stone-200 uppercase"
+          type="number"
+          placeholder="amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+          id={amountInputId}
+        />
       </div>
       <div className="w-1/2 flex flex-col gap-4 justify-end text-right items-end">
         <p>Currency Type</p>
-        <select className="bg-stone-200 px-3 py-2 w-fit rounded-lg">
-          <option value="usd">usd</option>
+        <select
+          className="bg-stone-200 px-3 py-2 w-fit rounded-lg uppercase"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+          {currencyOptions.map(currency => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
         </select>
       </div>
     </div>
